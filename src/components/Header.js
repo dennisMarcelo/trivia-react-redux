6db2251061a91fb6a3d18102a86d4be556185c4a
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-06-15 15:30:34 -0300

    atualizaçao do hader

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends React.Component {
   render() {
+    const { img, name } = this.props;
     return (
       <section>
-        <img src="" alt="" data-testid="header-profile-picture" />
-        <p data-testid="header-player-name">Name:</p>
+        <img src={ img } alt="Token" data-testid="header-profile-picture" />
+        <p data-testid="header-player-name">{ name }</p>
         <p data-testid="header-score">Pontuacao</p>
       </section>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  img: state.user.image,
+  name: state.user.name,
+});
+
+Header.propTypes = {
+  img: PropTypes.shape(Object).isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Header);
