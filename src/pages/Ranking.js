c6851c25d92c3f819ba123bbec4e2b748a28bc7e
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-06-16 18:40:26 -0300

    requisito 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
   render() {
+    const { history } = this.props;
     return (
       <section>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Início
+        </button>
       </section>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape(Object).isRequired,
+};
 
 export default Ranking;
