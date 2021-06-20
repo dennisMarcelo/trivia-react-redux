86cfdea3e4209747b661624de6bb9c802a31234f
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-06-20 18:07:22 -0300

    Alteracoes CSS

--- src/pages/Config.js
+++ src/pages/Config.js
@@ -1,15 +1,45 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import '../css/Config.css';
+import actionSelectedCategory from '../Redux/action/actionCategory';
 
 class Config extends React.Component {
+  constructor() {
+    super();
+    this.state = { selectedCategory: 'Enterteinment: Music' };
+    this.handleSelectedCategory = this.handleSelectedCategory.bind(this);
+  }
+
+  handleSelectedCategory({ target: { value } }) {
+    const { goCategory } = this.props;
+    this.setState({ selectedCategory: value }, () => {
+      const { selectedCategory } = this.state;
+      goCategory(selectedCategory);
+    });
+  }
+
   render() {
     return (
       <div className="config-container">
         <h1 data-testid="settings-title">Configuração</h1>
-        <p>teste- depois apagar</p>
+        <label htmlFor="category">
+          Selecione a Categoria:
+          <select id="category" onChange={ this.handleSelectedCategory }>
+            <option>Enterteinment: Music</option>
+            <option>Science and Nature</option>
+            <option>Science: Computers</option>
+            <option>Video Games</option>
+            <option>Board Games</option>
+            <option>Japonese Anime and Manga</option>
+          </select>
+        </label>
       </div>
     );
   }
 }
 
-export default Config;
+const mapDispatchToProps = (dispatch) => ({
+  goCategory: (category) => dispatch(actionSelectedCategory(category)),
+});
+
+export default connect(null, mapDispatchToProps)(Config);
